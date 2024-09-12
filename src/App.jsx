b98c527c1a67141aa3814637c94cd10610284e69
saad ahmed <ss5099903@gmail.com>
2024-09-12 23:35:10 +0500

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,56 @@
-// import React, { useRef, useState } from 'react'
-// import axios  from 'axios'
-// import { Card } from './assets/card.jsx'
-// import "./App.css"
+import React, { useRef, useState } from 'react'
+import axios  from 'axios'
+import { Card } from './assets/card.jsx'
+import "./App.css"
 
-// const App = () => {
+const App = () => {
 
-//   const [data, setdata ] = useState([])
-//   const [loading, setloading ] = useState()
-//   const city = useRef()
-//   console.log(data);
-//   function weatherapp(event){
-//     event.preventDefault()
-//     console.log(city.current.value);
-//     setloading(true)
+  const [data, setdata ] = useState([])
+  const [loading, setloading ] = useState()
+  const city = useRef()
+  console.log(data);
+  function weatherapp(event){
+    event.preventDefault()
+    console.log(city.current.value);
+    setloading(true)
  
-//     const key = 'b4db485d7c4c485fa6d84351232508'
-//     axios.get(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${city.current.value}&aqi=no`)
-//       .then((res) => {
-//         console.log('res==>', res.data);
-//         setdata([res.data, ...data]);
-//         city.current.value = '';
-//         setloading(false)
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//         alert('no such city');
-//         city.current.value = '';
-//         setloading(false)
-//       })
+    const key = 'b4db485d7c4c485fa6d84351232508'
+    axios.get(`https://api.weatherapi.com/v1/current.json?key=${key}&q=${city.current.value}&aqi=no`)
+      .then((res) => {
+        console.log('res==>', res.data);
+        setdata([res.data, ...data]);
+        city.current.value = '';
+        setloading(false)
+      })
+      .catch((err) => {
+        console.log(err);
+        alert('no such city');
+        city.current.value = '';
+        setloading(false)
+      })
 
-//   }
+  }
 
-//   return (
-    
-//     <div className='main text-center'>
-      
-// <div className='input'>
-// <h1>Weather App</h1>
-// <form onSubmit={weatherapp}>
-//   <input type="text" ref={city} placeholder='Enter City Name'/>
-//   <button type='submit'>{loading ? "loading..." :"Check"}</button>
-// </form>
-// {/* <Card/> */}
-// {data.length>0? data.map((item , index)=>{
-//   return(
-//   <Card key={index} name={item.location.name} temperature={item.current.temp_c} src={item.current.condition.icon}  date={item.location.localtime} weatherText={item.current.condition.text} country={item.location.country}/>
-//   )
-// }) : <h1>Search Weather..</h1>}
-// </div>
-//     </div>
-//   )
-// }
-
-// export default App
-function App() {
   return (
-    <div className="text-center">
-      <h1 className="text-3xl font-bold underline text-blue-500">
-        Hello, Tailwind CSS!
-      </h1>
+    
+    <div className='main text-center'>
+      
+<div className='inp mt-5'>
+<h1 className='text-4xl m-2'>Weather App</h1>
+<form onSubmit={weatherapp}>
+  <input className='input' type="text" ref={city} placeholder='Enter City Name'/>
+  <button type='submit' className='btn btn-info'>{loading ? "loading..." :"Check"}</button>
+</form>
+{/* <Card/> */}
+{data.length>0? data.map((item , index)=>{
+  return(
+  <Card key={index} name={item.location.name} temperature={item.current.temp_c} src={item.current.condition.icon}  date={item.location.localtime} weatherText={item.current.condition.text} country={item.location.country}/>
+  )
+}) : <h1 className='text-4xl m-2'>Search Weather..</h1>}
+</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
--- src/assets/card.jsx
+++ src/assets/card.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 export const Card = ({name,date,country,temperature,src,weatherText}) => {
   return (
-    <div className=' bg-white border-gray-800 shadow-lg ring-gray-700 ring-opacity-50 w-[50%] mx-auto mt-5 rounded-lg p-[1rem] mb-5'>
+    <div className=' bg-white border-gray-800 shadow-lg ring-gray-700 ring-opacity-50 w-[auto] mx-auto mt-5 rounded-lg p-[1rem] mb-5'>
     <h1 className='text-2xl'>City:{name}</h1>
     <p className='text-gray-500'> Date :{date} , {country}</p>
     <div className='mt-5 flex justify-between items-center mx-5 pb-[1rem]'>
-        <h2 className='text-4xl md:text-7xl lg:text-8xl '>Temprature:{temperature}°C</h2>
+        <p className='text-2xl md:text-3xl lg:text-5xl '>{temperature}°C</p>
         <img className='w-[10rem]' src={src} alt='weatherImg' />
     </div>
     <p className='text-1xl'>Weather Condition :{weatherText}</p>
